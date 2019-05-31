632b75ca375176e24f69e5b7ca28d5ab8047424e
Darío Carrasquel <addario7777@gmail.com>
2019-05-31 10:00:47 -0500

    Test 1

--- src/main/java/com/company/events/application/EventsApplication.java
+++ src/main/java/com/company/events/application/EventsApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.PropertySources;
 public class EventsApplication {
     public static final String API_V1 = "/api/v1";
     public static void main(String[] args) {
+    	// Application starts
         SpringApplication.run(EventsApplication.class, args);
     }
 }
